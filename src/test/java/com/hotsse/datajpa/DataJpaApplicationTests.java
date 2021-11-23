97adf1086eabd25488007d9fe7e71f2ae8c1e0e2
Hotsse <dkdlrja@naver.com>
2021-11-23 23:01:33 +0900

    project initialize

---
+++ src/test/java/com/hotsse/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotsse.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
